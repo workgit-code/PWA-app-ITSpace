8781c3baa28cee1a2d96bcb05b1ca5f55e1d5255
Sesil <s.tasim@student.fontys.nl>
2022-04-08 09:24:36 +0200

    Added planet one

--- learning-app/src/App.js
+++ learning-app/src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
-import {Reat, useState, useEffect} from 'react'
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./App.css";
+import Sphere from "./components/Sphere";
+import SkyBox from "./components/SkyBox";
+import CameraControls from "./components/CameraControls";
 
 function App() {
-  
-  
-
- 
-
   return (
     <div className="App">
-      
+      <Canvas className="canvas">
+        <CameraControls />
+        <directionalLight intensity={1} />
+        <ambientLight intensity={0.6} />
+        <Suspense fallback="loading">
+          <Sphere />
+        </Suspense>
+        <SkyBox />
+      </Canvas>
     </div>
   );
 }
 
-export default App
+export default App;
