f5332ad494d7d9ca6b49fe9995903d51d67559c7
Sesil <s.tasim@student.fontys.nl>
2022-04-08 12:36:36 +0200

    Planets UI images

--- learning-app/src/App.js
+++ learning-app/src/App.js
@@ -1,22 +1,14 @@
 import React, { Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
-import Sphere from "./components/Sphere";
-import SkyBox from "./components/SkyBox";
-import CameraControls from "./components/CameraControls";
+import Planets from "./components/Planets";
+// import SkyBox from "./components/SkyBox";
+// import CameraControls from "./components/CameraControls";
 
 function App() {
   return (
     <div className="App">
-      <Canvas className="canvas">
-        <CameraControls />
-        <directionalLight intensity={1} />
-        <ambientLight intensity={0.6} />
-        <Suspense fallback="loading">
-          <Sphere />
-        </Suspense>
-        <SkyBox />
-      </Canvas>
+      <Planets />
     </div>
   );
 }
